05ff6424e4686912dc81a23eee9a26a0244afe06
hyeon <hyeon@192.168.0.21>
2023-11-22 18:50:42 +0900

    자바클래스01

---
+++ test/src/ch01/VarEx01.java
@@ -0,0 +1,14 @@
+package ch01;
+// 패키지 = 자바 파일이 모여 있는 폴더
+
+public class VarEx01 {
+
+	public static void main(String[] args) {
+		boolean b1 = true; // 박스에 1이 저장됨
+		boolean b2 = false; // 박스 0
+		int n1 = 1000;
+		double d1 = 1000.1;
+		char c1 = '가';
+	}
+
+}
